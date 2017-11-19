1056239e7ad5568ba46dabcd249984f081a0b30f
shubhamjha97 <jha1shubham@gmail.com>
2017-11-19 21:32:48 +0530

    'final'

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ def home():
 def get_output_file(name):
 	# print 'adfsdfsdffdsf'
 	print 'file-name', name
-	# file_name=app.root_path+'\\jobs\\'+name+'\\model\\'+name+'.pkl'
-	# file_name=app.root_path+'\\tmp\\'+name+'\\model\\'+name+'.pkl'
-	file_name=app.root_path+'\\tmp\\'+name+'.pkl'
+	# file_name=app.root_path+'/jobs/'+name+'/model/'+name+'.pkl'
+	# file_name=app.root_path+'/tmp/'+name+'/model/'+name+'.pkl'
+	file_name=app.root_path+'/tmp/'+name+'.pkl'
 	print 'f name', file_name
 	return send_file(file_name, as_attachment=True)
 
@@ -35,9 +35,9 @@ def fetch_pred():
 		print 'job_name', job_name
 		print request.files.keys()
 		pred_file=request.files['to_predict']
-		pred_file.save(app.root_path+'\\tmp\\data_'+job_name)
-		model_path=app.root_path+'\\tmp\\'+job_name+'.pkl'
-		data_path=app.root_path+'\\tmp\\data_'+job_name
+		pred_file.save(app.root_path+'/tmp/data_'+job_name)
+		model_path=app.root_path+'/tmp/'+job_name+'.pkl'
+		data_path=app.root_path+'/tmp/data_'+job_name
 		header=None
 		try:
 			final_pred_array=make_predictions(model_path, data_path, header)
@@ -45,39 +45,32 @@ def fetch_pred():
 			error_msg="Incorrect data file selected. This model wasn't trained on this data format."
 			# return ("Incorrect data file selected. This model wasn't trained on this data format.")
 			return render_template('error_page.html', msg=error_msg)
-		final_path=app.root_path+'\\tmp\\'+'predictions_'+job_name+'.txt'
+		final_path=app.root_path+'/tmp/'+'predictions_'+job_name+'.txt'
 		np.savetxt(final_path, final_pred_array)
 		return send_file(final_path, as_attachment=True)
 
 @app.route('/formSubmit', methods=['POST'])
 def submit():
 	if(request.method == 'POST'):
+
 		f = request.files['file']
 		job_name=request.form['job_name']+'_'+randomword(5)
-		# job_path=app.root_path+'\\jobs\\'+job_name
-		# job_path=app.root_path+'\\tmp\\'#+job_name
+
+		# job_path=app.root_path+'/tmp/'#+job_name
 		job_path=app.root_path
-		model_path=job_path+'\\tmp\\'+job_name+'.pkl'
-		# print job_path, model_path
-		# if not os.path.exists(job_path):
-		# 	os.makedirs(job_path)
-		# 	os.makedirs(job_path+'\\data')
-		# 	os.makedirs(job_path+'\\model')
-		# print 'job_path',job_path
-		# f.save(job_path+'\\data\\'+'data')#secure_filename(f.filename))
-		
-		f.save(app.root_path+'\\tmp\\data_'+job_name)#secure_filename(f.filename))
+		# model_path=job_path+'/tmp/'+job_name+'.pkl'
+		model_path=job_path+'/tmp/'+job_name+'.pkl'
+
+		# f.save(app.root_path+'/tmp/data_'+job_name)
+		f.save(app.root_path+'/tmp/data_'+job_name)
 		try:
 			filename, metric_value=process(request.form, job_path, job_name)
 		except ValueError:
 			error_msg='Target data is non numeric. Regression requires target to be numeric.'
-			# return ('Target data is non numeric. Regression requires target to be numeric.')
 			return render_template('error_page.html', msg=error_msg)
 		except KeyError:
 			error_msg="The entered column doesn't exist. Please note that the columns are 0 indexed."
 			return render_template('error_page.html', msg=error_msg)
-			# return "The entered column doesn't exist. Please note that the columns are 0 indexed."
-		# print filename, metric_value
 		if request.form['train_type']=='reg':
 			METRIC_TYPE='RMSE'
 		else:
@@ -91,18 +84,11 @@ def train_on_model():
 	temp_data=request.files['to_predict']
 
 	job_name=request.form['job_name']+'_'+randomword(5)
-	# job_path=app.root_path+'\\jobs\\'+job_name
-	# job_path=app.root_path+'\\tmp\\'+job_name
 
-	# if not os.path.exists(job_path):
-	# 		os.makedirs(job_path)
-	# 		os.makedirs(job_path+'\\model')
-	# 		os.makedirs(job_path+'\\data')
-
-	# model_path=job_path+'\\model\\'+job_name+'.pkl'
-	model_path=app.root_path+'\\tmp\\'+job_name+'.pkl'
-	# data_path=job_path+'\\data\\'+'data'
-	data_path=app.root_path+'\\tmp\\'+'data_'+job_name
+	# model_path=job_path+'/model/'+job_name+'.pkl'
+	model_path=app.root_path+'/tmp/'+job_name+'.pkl'
+	# data_path=job_path+'/data/'+'data'
+	data_path=app.root_path+'/tmp/'+'data_'+job_name
 
 	temp_model.save(model_path)
 	temp_data.save(data_path)
@@ -118,15 +104,15 @@ def train_on_model():
 	except ValueError:
 		error_msg="This model is not comptable with the test data selected. This model wasn't trained on this data format."
 		return render_template('error_page.html', msg=error_msg)
-		return ("This model is not comptable with the test data selected. This model wasn't trained on this data format.")
-	final_path=app.root_path+'\\tmp\\'+'predictions_'+job_name+'.txt' #job_path+'\\pred\\predictions.txt'
+		# return ("This model is not comptable with the test data selected. This model wasn't trained on this data format.")
+	final_path=app.root_path+'/tmp/'+'predictions_'+job_name+'.txt' #job_path+'/pred/predictions.txt'
 	np.savetxt(final_path, final_pred_array)
 	return send_file(final_path, as_attachment=True)
 
 @app.route('/jobFinished', methods=['POST'])
 def finish():
 	job_name=request.form['job_name']
-	# shutil.rmtree(app.root_path+'\\jobs\\'+job_name)
+	# shutil.rmtree(app.root_path+'/jobs/'+job_name)
 	return render_template('index_new.html')
 
 if __name__ == '__main__':

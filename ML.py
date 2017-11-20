2374ae12aee7faf072a2a4c911d92a08ca7e6471
shubhamjha97 <jha1shubham@gmail.com>
2017-11-20 12:12:13 +0530

    fixed classification

--- ML.py
+++ ML.py
@@ -4,7 +4,6 @@ from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.metrics import accuracy_score, mean_squared_error
 import os
 import pandas as pd
-# import pickle
 import cPickle as pickle
 import numpy as np
 import time
@@ -19,40 +18,34 @@ algo_dict={'rf_cla':RandomForestClassifier,
 	 'dt_reg':DecisionTreeRegressor}
 
 def process(param_dict, root_path, job_name):
-	# print 'path:', job_path
-	# print 'Entered process'
-	# data_path=job_path+'\\data\\data'
-	# data_path=root_path+'\\tmp\\data_'+job_name
 	data_path=root_path+'/tmp/data_'+job_name
-	# model_path=job_path+'\\model\\'+job_name+'.pkl'
-	# model_path=root_path+'\\tmp\\'+job_name+'.pkl'
 	model_path=root_path+'/tmp/'+job_name+'.pkl'
-	# print 'data path:', data_path
-	# print param_dict.keys()
 	if len(param_dict['header_row'])==0:
 		header_row=None
 	else:
 		header_row=int(param_dict['header_row'])
-	# print 'data path:', data_path
 	df=pd.read_table(data_path, sep=None, header=header_row, engine='python')
-	# print 'data loaded'
 	df = df.sample(frac=1).reset_index(drop=True)
 	NO_TRAIN=int(TRAIN_PERCENTAGE*df.shape[0])
 	NO_TEST=int(df.shape[0]-NO_TRAIN)
+	algo_name=param_dict['algorithm']+'_'+param_dict['train_type']
+
 	train=df.head(NO_TRAIN)
 	test=df.tail(NO_TEST)
-	algo_name=param_dict['algorithm']+'_'+param_dict['train_type']
-	#algorithm, train_type, job_name, y_col, file, train_type
-	y=df[int(param_dict['y_col'])]
+	y_train=train[int(param_dict['y_col'])]
+	y_test=test[int(param_dict['y_col'])]
 	y_map={}
-	for ix, y_val in enumerate(y.unique()):
-		y_map[y_val]=ix
-	y=y.apply(lambda row: y_map[row])
-	y_train=df[int(param_dict['y_col'])].head(NO_TRAIN)
-	y_test=df[int(param_dict['y_col'])].tail(NO_TEST)
-	X=df.drop(int(param_dict['y_col']), axis=1)
-	X_train=X.head(NO_TRAIN)
-	X_test=X.tail(NO_TEST)
+	if param_dict['train_type']=='cla':
+		for ix, y_val in enumerate(y.unique()):
+			y_map[y_val]=ix
+		y_train=y_train.apply(lambda row: y_map[row])
+		y_test=y_test.apply(lambda row: y_map[row])
+	# y_train=df[int(param_dict['y_col'])].head(NO_TRAIN)
+	# y_test=df[int(param_dict['y_col'])].tail(NO_TEST)
+	# X=df.drop(int(param_dict['y_col']), axis=1)
+	X_train=train.drop(int(param_dict['y_col']), axis=1)#X.head(NO_TRAIN)
+	X_test=test.drop(int(param_dict['y_col']), axis=1)
+
 	model=algo_dict[algo_name]()
 	model.fit(X_train, y_train)
 	predictions=model.predict(X_test)
@@ -60,19 +53,15 @@ def process(param_dict, root_path, job_name):
 	if param_dict['train_type']=='cla':
 		acc=accuracy_score(y_test, predictions)
 		metric_value=acc
-		# print 'acc', metric_value
 	else:
 		rmse=np.sqrt(mean_squared_error(y_test, predictions))
 		metric_value=rmse
-		# print 'rmse', metric_value
 	with open(model_path, 'w') as f:
 		pickle.dump(model, f)
-		# print 'model saved'
 	return root_path, metric_value
 
 def make_predictions(model_path, data_path, header):
 	X=pd.read_table(data_path, sep=None, header=header, engine='python')
-	#with open(model_path, 'rb') as f:
 	model=pickle.load(open(model_path, 'r'))
 	pred=model.predict(X)
 	return pred
